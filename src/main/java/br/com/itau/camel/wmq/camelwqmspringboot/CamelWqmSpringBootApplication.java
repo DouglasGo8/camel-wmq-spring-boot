8e8f310965bede9bfb13f3d36d245b878b42849d
Douglas Dias Batista <douglasdiasbatista@Douglass-MacBook-Pro.local>
2020-02-14 22:41:13 -0300

    First Commit

---
+++ src/main/java/br/com/itau/camel/wmq/camelwqmspringboot/CamelWqmSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.itau.camel.wmq.camelwqmspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelWqmSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelWqmSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/itau/camel/wmq/camelwqmspringboot/CamelWqmSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.itau.camel.wmq.camelwqmspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelWqmSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
